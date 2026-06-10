93920e03d771e781cfa7a3bc63879e9b3ea302c0
sebastiansperandio <sebastiansperandio@gmail.com>
2026-06-10 16:18:43 -0300

    Update exercise 2

--- src/components/EtiquetaCategoria.jsx
+++ src/components/EtiquetaCategoria.jsx
@@ -5,10 +5,20 @@
 //   - color  (string, un color CSS válido)
 //
 // Debe mostrar el nombre como una etiqueta/badge con el color de fondo recibido.
-// Aplicale estilos para que se vea como una etiqueta (padding, border-radius, etc.)
+// Aplicale estilos para que se vea como una etiqueta:
+/* style={{
+		display: 'inline-block',
+		color: '#fff',
+		padding: '3px 10px',
+		borderRadius: '999px',
+		fontSize: '0.8rem',
+		fontWeight: 'bold',
+		marginRight: '6px',
+		marginBottom: '6px',
+	  }} */
 
 function EtiquetaCategoria() {
-  return null // TODO: reemplazá esto con tu implementación
+	return null // TODO: reemplazá esto con tu implementación
 }
 
 export default EtiquetaCategoria
